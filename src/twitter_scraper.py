6e7b9237e7cde47d3dfc2d9fe74d75dfa99a86cf
baniasbaabe <72874670+baniasbaabe@users.noreply.github.com>
2022-08-26 21:21:14 +0200

    Refactor code

--- src/twitter_scraper.py
+++ src/twitter_scraper.py
@@ -1,16 +1,28 @@
+import pandas as pd
 import snscrape.modules.twitter as sntwitter
 
-query = "sengoku degens OR #sengokudegens"
+def get_tweet_items(query):
+    return sntwitter.TwitterSearchScraper(query).get_items()
 
-tweets = []
+def scrape_tweets(query):
+    tweets_list = []
+    tweets = get_tweet_items(query)
+    for i, tweet in enumerate(tweets):
+        if i > 5:
+            break
+        else:
+            tweets_list.append([tweet.date, tweet.id, tweet.sourceLabel, tweet.content, tweet.url, tweet.user.id, \
+                        tweet.user.username, tweet.user.followersCount, tweet.replyCount,\
+                        tweet.retweetCount, tweet.likeCount, tweet.quoteCount, tweet.lang,\
+                        tweet.outlinks, tweet.retweetedTweet, tweet.quotedTweet,\
+                        tweet.inReplyToTweetId, tweet.inReplyToUser, tweet.mentionedUsers,\
+                        tweet.coordinates, tweet.place, tweet.hashtags, tweet.cashtags])
 
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
-    if i > 1000:
-        break
-    else:
-        tweets.append([tweet.date, tweet.id, tweet.sourceLabel, tweet.content, tweet.url, tweet.user.id, \
-                      tweet.user.username, tweet.user.followersCount, tweet.replyCount,\
-                      tweet.retweetCount, tweet.likeCount, tweet.quoteCount, tweet.lang,\
-                      tweet.outlinks, tweet.retweetedTweet, tweet.quotedTweet,\
-                      tweet.inReplyToTweetId, tweet.inReplyToUser, tweet.mentionedUsers,\
-                      tweet.coordinates, tweet.place, tweet.hashtags, tweet.cashtags])
+    return tweets_list
+
+def create_dataframe_from_tweets_list(tweets_list):
+    return pd.DataFrame(tweets_list)
+
+if __name__ == "__main__":
+    tweets = scrape_tweets("sengoku degens")
+    df = create_dataframe_from_tweets_list(tweets)
