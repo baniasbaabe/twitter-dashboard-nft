40a9bbf14b02b7506d5cf4679dc826c7a2c03b36
baniasbaabe <72874670+baniasbaabe@users.noreply.github.com>
2022-10-01 15:59:53 +0200

    Add sentiment chart

--- src/app.py
+++ src/app.py
@@ -8,15 +8,19 @@ import re
 import string
 import nltk
 import emoji
+import plotly.graph_objects as go
 nltk.download("stopwords")
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = dash.Dash(__name__)
 
 df = pd.read_csv(r"C:\Code\twitter-dashboard-nft\src\test_df.csv")
 
 stop_words = list(stopwords.words("english"))
+
+sid_obj = SentimentIntensityAnalyzer()
 
 def preprocess_tweet(text):
     text = text.lower()
@@ -32,11 +36,23 @@ def preprocess_tweet(text):
     text = " ".join(no_stops)
     return text
 
-text = df["Content"].apply(preprocess_tweet)
+def apply_sentiment(tweet):
+    return sid_obj.polarity_scores(tweet).get("compound")
+
+def bin_sentiment(sentiment):
+    return "Negative" if sentiment < 0.5 else "Positive"
+
+df["Content_Preprocessed"] = df["Content"].apply(preprocess_tweet)
+
+df["Sentiment"] = df["Content_Preprocessed"].apply(apply_sentiment)
+
+df["Sentiment_String"] = df["Sentiment"].apply(bin_sentiment)
 
 df_most_tweets_by_user = df.groupby("Username")["ID"].count().nlargest(5)
 
-series_most_frequent_words = pd.Series(' '.join(df['Content']).lower().split()).value_counts()[:20].to_frame("Count")
+series_most_frequent_words = pd.Series(' '.join(df['Content_Preprocessed']).lower().split()).value_counts()[:20].to_frame("Count")
+
+df_sentiment = df.groupby("Sentiment_String").size().to_frame("Count")
 
 fig_most_tweets_by_user = px.bar(df_most_tweets_by_user, x=df_most_tweets_by_user.index, y="ID", text_auto=True)
 
@@ -45,7 +61,12 @@ fig_geo = px.scatter_geo(df, lat="Latitude", lon="Longitude",
                      hover_name="Country", 
                      projection="natural earth")
 
-fig_most_frequent_words = px.bar(series_most_frequent_words, x = series_most_frequent_words.index, y = "Count")
+fig_most_frequent_words = px.bar(series_most_frequent_words, x = series_most_frequent_words.index, y = "Count", text_auto=True)
+
+fig_sentiment = px.bar(df_sentiment, x = df_sentiment.index, y = "Count", text_auto=True)
+
+
+print(df["Sentiment"].head())
 
 app.layout = html.Div(className='row', children=[
     html.H1("NFT Trend"),
@@ -55,7 +76,8 @@ app.layout = html.Div(className='row', children=[
         dcc.Graph(id="geo-map", style={'display': 'inline-block'}, figure=fig_geo)
     ]),
     html.Div(children=[
-        dcc.Graph(id="most-tweets-by-usser", style={'display': 'inline-block'}, figure=fig_most_frequent_words),
+        dcc.Graph(id="mos", style={'display': 'inline-block'}, figure=fig_most_frequent_words),
+        dcc.Graph(id="most-twee-usser", style={'display': 'inline-block'}, figure=fig_sentiment),   
     ])
     
 ])
