d798cbee454adc37a9440c4c73a1c93d2b073274
baniasbaabe <72874670+baniasbaabe@users.noreply.github.com>
2022-10-02 19:12:42 +0200

    Add callback to make custom inputs

--- src/app.py
+++ src/app.py
@@ -1,87 +1,142 @@
-from multiprocessing.resource_sharer import stop
+import re
+import string
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import numpy as np
 import pandas as pd
 import plotly.express as px
-import re
-import string
-import nltk
-import emoji
 import plotly.graph_objects as go
-nltk.download("stopwords")
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from dash.dependencies import Input, Output, State
+
+from preprocess import preprocess_pipeline
+from twitter_scraper import create_dataframe_from_tweets_list, scrape_tweets
 
 app = dash.Dash(__name__)
+app.title = "NFT Tracking Dashboard"
 
-df = pd.read_csv(r"C:\Code\twitter-dashboard-nft\src\test_df.csv")
+app.layout = html.Div(
+    className="row",
+    children=[
+        html.H1("NFT Trend", style={"text-align": "center"}),
+        html.P(
+            children="Analyze the tweets and sentiment of a NFT collection",
+            style={"text-align": "center"},
+        ),
+        html.Br(),
+        html.P(
+            children="When trading with Non-Fungible-Tokens (NFT) one important aspect to stay updated on Twitter. On Twitter, NFT-influencer can share their opinion and maybe influence a NFT collection. E.g. a big influencer (and CEO of DeGods) named Frank can talk in a positive way about a NFT collection and consequently increasing the floor price.",
+            style={"text-align": "center"},
+        ),
+        html.Br(),
+        html.P(
+            children="With this dashboard you have a quick overview over the collection XYZ to gain insights and use it as a decision support when trading with NFTs",
+            style={"display": "block", "margin-right": "auto", "margin-left": "auto"},
+        ),
+        html.P(
+            children="Type in a NFT collection and wait for the results*",
+            style={"display": "block", "margin-right": "auto", "margin-left": "auto"},
+        ),
+        dcc.Input(
+            id="input-handle",
+            type="text",
+            placeholder="NFT Collection",
+            value="degods",
+            style={"text-align": "center"},
+        ),
+        html.Button(id="hit-button", children="submit", style={"text-align": "center"}),
+        dcc.Loading(
+            id="loading-1",
+            type="default",
+            children=[
+                html.Div(
+                    children=[
+                        dcc.Graph(
+                            id="most-tweets-by-user",
+                            style={"display": "inline-block"},
+                            figure={},
+                        ),
+                        dcc.Graph(
+                            id="geo-map", style={"display": "inline-block"}, figure={}
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        dcc.Graph(
+                            id="most-frequent-words",
+                            style={"display": "inline-block"},
+                            figure={},
+                        ),
+                        dcc.Graph(
+                            id="sentiment",
+                            style={"display": "inline-block"},
+                            figure={},
+                        ),
+                    ]
+                ),
+                html.P(
+                    children="*Since snscrape is used as the Twitter Scraper, it could be a bit time consuming for scraping the tweets.",
+                    style={"text-align": "left"},
+                ),
+            ],
+        ),
+    ],
+)
 
-stop_words = list(stopwords.words("english"))
 
-sid_obj = SentimentIntensityAnalyzer()
+@app.callback(
+    Output(component_id="most-tweets-by-user", component_property="figure"),
+    Output(component_id="geo-map", component_property="figure"),
+    Output(component_id="most-frequent-words", component_property="figure"),
+    Output(component_id="sentiment", component_property="figure"),
+    Input(component_id="hit-button", component_property="n_clicks"),
+    State(component_id="input-handle", component_property="value"),
+)
+def display_value(nclicks, count_handle):
 
-def preprocess_tweet(text):
-    text = text.lower()
-    text = re.sub(r"(@[A-Za-z0-9]+)|(RT\s?@[A-Za-z0-9])", "", text) # Remove Mentions and Retweet
-    text = re.sub(r"https?:\/\/\S*", "", text) # Remove links
-    # Remove duplicated words
-    words = text.split()
-    text = " ".join(sorted(set(words), key=words.index)) 
-    text = text.translate(str.maketrans('', '', string.punctuation + string.digits)) # Remove digits and punctuation
-    text = emoji.replace_emoji(text, "")
-    tokens = word_tokenize(text)
-    no_stops = [word for word in tokens if word not in stop_words]
-    text = " ".join(no_stops)
-    return text
+    tweets = scrape_tweets(count_handle, n=1000)
+    df = create_dataframe_from_tweets_list(tweets)
 
-def apply_sentiment(tweet):
-    return sid_obj.polarity_scores(tweet).get("compound")
+    df = preprocess_pipeline(df)
 
-def bin_sentiment(sentiment):
-    return "Negative" if sentiment < 0.5 else "Positive"
+    df_most_tweets_by_user = df.groupby("Username")["ID"].count().nlargest(5)
 
-df["Content_Preprocessed"] = df["Content"].apply(preprocess_tweet)
+    series_most_frequent_words = (
+        pd.Series(" ".join(df["Content_Preprocessed"]).lower().split())
+        .value_counts()[:20]
+        .to_frame("Count")
+    )
 
-df["Sentiment"] = df["Content_Preprocessed"].apply(apply_sentiment)
+    df_sentiment = df.groupby("Sentiment_String").size().to_frame("Count")
 
-df["Sentiment_String"] = df["Sentiment"].apply(bin_sentiment)
+    fig_most_tweets_by_user = px.bar(
+        df_most_tweets_by_user, x=df_most_tweets_by_user.index, y="ID", text_auto=True
+    )
 
-df_most_tweets_by_user = df.groupby("Username")["ID"].count().nlargest(5)
+    fig_geo = px.scatter_geo(
+        df,
+        lat="Latitude",
+        lon="Longitude",
+        color="Country",
+        hover_name="Country",
+        projection="natural earth",
+    )
 
-series_most_frequent_words = pd.Series(' '.join(df['Content_Preprocessed']).lower().split()).value_counts()[:20].to_frame("Count")
+    fig_most_frequent_words = px.bar(
+        series_most_frequent_words,
+        x=series_most_frequent_words.index,
+        y="Count",
+        text_auto=True,
+    )
 
-df_sentiment = df.groupby("Sentiment_String").size().to_frame("Count")
+    fig_sentiment = px.bar(
+        df_sentiment, x=df_sentiment.index, y="Count", text_auto=True
+    )
 
-fig_most_tweets_by_user = px.bar(df_most_tweets_by_user, x=df_most_tweets_by_user.index, y="ID", text_auto=True)
-
-fig_geo = px.scatter_geo(df, lat="Latitude", lon="Longitude",
-                     color="Country",
-                     hover_name="Country", 
-                     projection="natural earth")
-
-fig_most_frequent_words = px.bar(series_most_frequent_words, x = series_most_frequent_words.index, y = "Count", text_auto=True)
-
-fig_sentiment = px.bar(df_sentiment, x = df_sentiment.index, y = "Count", text_auto=True)
-
-
-print(df["Sentiment"].head())
-
-app.layout = html.Div(className='row', children=[
-    html.H1("NFT Trend"),
-    html.P(children="Analyze the tweets and sentiment of a NFT collection",),
-    html.Div(children=[
-        dcc.Graph(id="most-tweets-by-user", style={'display': 'inline-block'}, figure=fig_most_tweets_by_user),
-        dcc.Graph(id="geo-map", style={'display': 'inline-block'}, figure=fig_geo)
-    ]),
-    html.Div(children=[
-        dcc.Graph(id="mos", style={'display': 'inline-block'}, figure=fig_most_frequent_words),
-        dcc.Graph(id="most-twee-usser", style={'display': 'inline-block'}, figure=fig_sentiment),   
-    ])
-    
-])
-
+    return fig_most_tweets_by_user, fig_geo, fig_most_frequent_words, fig_sentiment
 
 
 if __name__ == "__main__":
